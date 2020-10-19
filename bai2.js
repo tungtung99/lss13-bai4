7e4b2445e36245164db444060e4373309a07ae48
tungtung99 <tungnguyen99hn1@gmail.com>
2020-10-19 23:14:34 +0700

    hoanthanhbai2

---
+++ bai2.js
@@ -0,0 +1,10 @@
+function xulytuoi(){
+    let namsinh = prompt("Nhap nam sinh: ");
+    let tuoi = 2020-namsinh;
+    if(tuoi>=16){
+        alert("học sinh đủ điều kiện vào lớp 10")
+    }
+    else{
+        alert("học sinh không đủ điều kiện vào lớp 10")
+    }
+}
