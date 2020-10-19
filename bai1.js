d6230de515c0743bed70308d9a560e2287a5f75d
tungtung99 <tungnguyen99hn1@gmail.com>
2020-10-19 23:04:48 +0700

    hoanthanhbai1

---
+++ bai1.js
@@ -0,0 +1,11 @@
+function xuly2so(){
+    let a= prompt("Nhap a: ");
+    let b= prompt("Nhap b: ");
+    if(a%b==0)
+    {
+        alert("a chia het cho b");
+    }
+    else{
+        alert("a khong chia het cho b");
+    }
+}
